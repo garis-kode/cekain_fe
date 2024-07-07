9fc1764961acea4aa44ecfd99037fe4020489f28
fajar-dev <fajarrivaldi2015@gmail.com>
2024-07-07 21:41:06 +0700

    refac: auth route

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-100 min-h-screen bg-slate-100 flex justify-center">
-    <div class="md:min-w-[480px] md:max-w-[480px] max-w-full min-w-full bg-slate-50 md:p-6 p-3">
+    <div class="md:min-w-[480px] md:max-w-[480px] max-w-full min-w-full bg-slate-50 md:p-6 p-4">
       <NuxtPage />
     </div>
   </div>
