d8915457fb0e1f422e8e8921227665cbaf6bebd9
fajar-dev <fajarrivaldi2015@gmail.com>
2024-11-23 20:19:35 +0700

    fix: font

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-100 min-h-screen bg-slate-100 flex justify-center dark:bg-gray-900">
+  <div class="max-w-100 min-h-screen bg-slate-100 flex justify-center dark:bg-gray-900 font-inter">
     <div class="md:min-w-[480px] md:max-w-[480px] max-w-full min-w-full bg-slate-50 dark:bg-gray-800 md:p-6 p-4">
       <NuxtPage />
     </div>
