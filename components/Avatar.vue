7ef32d3072d1668bb46ab5d2e9819618c17e53f6
fajar-dev <fajarrivaldi2015@gmail.com>
2024-12-13 03:56:10 +0700

    fix: avatar color

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -1,8 +1,14 @@
 <template>
   <div
-    class="flex items-center justify-center font-semibold text-blue-700"
+    class="flex items-center justify-center font-semibold"
     :class="customClass"
-    :style="{ backgroundColor: backgroundColor, width: size, height: size, borderRadius: radius }"
+    :style="{ 
+      backgroundColor: backgroundColor, 
+      color: textColor, 
+      width: size, 
+      height: size, 
+      borderRadius: radius 
+    }"
   >
     {{ initials }}
   </div>
@@ -30,11 +36,35 @@ const props = defineProps({
   },
 });
 
+function stringToModerateBrightColor(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  const r = Math.floor(((hash >> 16) & 0xFF) * 0.5 + 150); 
+  const g = Math.floor(((hash >> 8) & 0xFF) * 0.5 + 150);
+  const b = Math.floor((hash & 0xFF) * 0.5 + 150);
+  
+  const moderateBrightColor = `#${Math.min(Math.max(r, 150), 200).toString(16).padStart(2, '0')}${Math.min(Math.max(g, 150), 200).toString(16).padStart(2, '0')}${Math.min(Math.max(b, 150), 200).toString(16).padStart(2, '0')}`;
+  
+  return moderateBrightColor;
+}
+
+function getTextColor(backgroundColor) {
+  const r = parseInt(backgroundColor.slice(1, 3), 16);
+  const g = parseInt(backgroundColor.slice(3, 5), 16);
+  const b = parseInt(backgroundColor.slice(5, 7), 16);
+  const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  return luminance > 128 ? '#000000' : '#FFFFFF'; 
+}
+
 const initials = computed(() => {
   const names = props.friendName.split(" ");
   return (names[0][0] + (names[1] ? names[1][0] : '')).toUpperCase();
 });
 
-const backgroundColor = computed(() => '#e4eff9');
-</script>
+const backgroundColor = computed(() => stringToModerateBrightColor(props.friendName));
 
+const textColor = computed(() => getTextColor("#fff"));
+</script>
